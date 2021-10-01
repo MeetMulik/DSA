6e832675eb4bb8f44dc69b1d718217f92df17535
MeetMulik <89148021+MeetMulik@users.noreply.github.com>
2021-10-01 16:04:46 +0530

    Doubly Circular Linked List

---
+++ Doubly Circular Linked List/DoublyCircularLL.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head, *tail;
+
+void create()
+{
+    int n;
+    printf("Enter the number of elements in Linked List :");
+    scanf("%d", &n);
+    struct node *newnode;
+    head = 0;
+    for (int i = 0; i < n; i++)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+
+        printf("Enter Data: ");
+        scanf("%d", &newnode->data);
+        if (head == 0)
+        {
+            head = tail = newnode;
+            head->next = head;
+            head->prev = head;
+        }
+        else
+        {
+            tail->next = newnode;
+            newnode->prev = tail;
+            newnode->next = head;
+            head->prev = newnode;
+            tail = newnode;
+        }
+    }
+}
+
+// void display()
+// {
+//     struct node *temp;
+//     temp = head;
+//     if (head == 0)
+//     {
+//         printf("List Empty...\n");
+//     }
+//     else
+//     {
+//         while (temp != tail)
+//         {
+//             printf("Element : %d\n", temp->data);
+//             temp = temp->next;
+//         }
+//         printf("Element : %d\n", temp->data);
+//     }
+// }
+
+void display()
+{
+    struct node *temp;
+    temp = head;
+    if (head == 0)
+    {
+        printf("List Empty...\n");
+    }
+    else
+    {
+        do
+        {
+            printf("Element :%d \n", temp->data);
+            temp = temp->next;
+        } while (temp != tail->next);
+    }
+}
+
+int main()
+{
+    create();
+    display();
+}
